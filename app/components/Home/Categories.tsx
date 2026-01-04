919b06dbc0a16af826fa2feb299afe0daa8d8327
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-01-04 16:48:24 +0600

    categories section done

--- app/components/Home/Categories.tsx
+++ app/components/Home/Categories.tsx
@@ -11,6 +11,10 @@ import p7 from "@/app/assets/p7.png";
 import Image from "next/image";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import l1 from "@/app/assets/l1.png";
+import l2 from "@/app/assets/l2.png";
+import l3 from "@/app/assets/l3.png";
+import l4 from "@/app/assets/l4.png";
 
 const categories = [
     { id: 1, image: p1, name: "Fruits" },
@@ -32,7 +36,7 @@ const responsive = {
 
 export default function Categories() {
     return (
-        <section className="py-12">
+        <section className="my-10">
             <div className="container mx-auto px-4">
                 <h2 className="text-xl lg:text-2xl font-semibold mb-10">
                     Categories
@@ -70,6 +74,37 @@ export default function Categories() {
                         </div>
                     ))}
                 </Carousel>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                <div className="bg-[#F0E8D5] flex flex-col items-center justify-center pb-2 rounded-lg    w-100 lg:min-w-80 mx-auto">
+                    <Image src={l1} alt="l1" />
+                    <Image src={l2} alt="l2" className="relative -top-10" />
+                    <p className="text-[14px] font-semibold text-center">
+                        Get the <span className="text-primary">fresh food</span>{" "}
+                        out of the farm
+                    </p>
+                </div>
+                <div className="bg-[#F0E8D5] w-100 lg:min-w-80 mx-auto p-5 space-y-3 relative rounded-lg flex flex-col">
+                    <p className="text-[27px] font-semibold">
+                        <span className="text-primary">Organic Fruits</span>{" "}
+                        Straight From Our Garden
+                    </p>
+                    <button className="btn w-fit bg-[#FF8080] text-white rounded-3xl">
+                        Order Now
+                    </button>
+                    <Image src={l3} alt="l3" className="relative lg:-bottom-5" />
+                </div>
+                <div className="bg-[#E7EAF3] w-100 lg:min-w-80 mx-auto p-5 space-y-3 rounded-lg relative flex flex-col justify-between">
+                    <h2 className="text-[25px] font-semibold">
+                        <span className="text-primary">Fresh <br /> Butchered</span>{" "} <br />
+                        Meat Direct <br /> To Your <br /> Home
+                    </h2>
+                    <button className="btn bg-[#5778DB] text-white rounded-3xl">
+                        Order Now
+                    </button>
+                    <Image src={l4} alt="l4" className="absolute right-10 top-1/4 w-40 object-cover" />
+                </div>
             </div>
         </section>
     );
